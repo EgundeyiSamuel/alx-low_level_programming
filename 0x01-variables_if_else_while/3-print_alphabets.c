474e5bdfdcdc079864da953999479c8e30de67d6
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-10-06 13:36:48 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main -  Prints the alphabet in lowercase, uppercase then and new line
+* Return: Returns (0) Sucess
+*/
+int main(void)
+{
+	char lower_case = 'a';
+	char capslk = 'A';
+
+	while (lowercase <= 'z')
+	{
+	putchar (lowercase);
+	lowercase++;
+	}
+	while (capslk <= 'Z')
+	{
+	putchar (capslk);
+	capslk++;
+	}
+	putchar('\n');
+	return (0);
+}
