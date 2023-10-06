9c9f6b789dc11dd0773c41e16b1bda8070e3ebd7
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-10-05 17:02:41 -0700

    hello world C.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Print an outcome
+ *Return: Returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a");
+	printf("piece of art,\n");
+	return (0);
+}
