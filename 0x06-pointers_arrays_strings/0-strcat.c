5be37d1e7a45df7b3cc2a80db001ebcf974fc616
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-10-19 05:21:14 -0700

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,28 @@
 #include "main.h"
 /**
-* _strcat - function appends the src string to the dest string
-*  
+ * _strcat - function appends the src string to the dest string
+ * @dest: destination's pointer
+ * @src: source string pointer
+ * Return: Returns a string dest with a given pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+
+while (src[i] != '\0')
+{
+i++;
+}
+while (dest[j] != '\0')
+{
+j++;
+}
+for (k = j; k < (i + j); k++)
+{
+dest[k] = src[k - j];
+}
+dest[k] = '\0';
+return (dest);
+}
