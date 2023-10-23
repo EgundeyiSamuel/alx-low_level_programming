f963389e76c17cfb4ef7c5007f72d2bfcc9b8449
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-10-23 14:31:23 -0700

    has been commited.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_chessboard - Print a chessboard.
- *
- * This function prints an 8x8 chessboard represented as a 2D array of characters.
- *
- * @a: The 2D array representing the chessboard.
- */
+* print_chessboard - Print a chessboard.
+*
+* This function prints an 8x8 chessboard represented.
+*
+* @a: The 2D array representing the chessboard.
+*/
 void print_chessboard(char (*a)[8])
 {
 	int row, column;
@@ -16,6 +16,6 @@ void print_chessboard(char (*a)[8])
 	{
 		_putchar(a[row][column]);
 	}
- 		_putchar('\n');
+		_putchar('\n');
 	}
 }
