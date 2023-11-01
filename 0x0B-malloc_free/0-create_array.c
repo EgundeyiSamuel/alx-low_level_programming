506434ea448c1d3d80d13e1254aa5cd1fef37706
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-10-31 17:35:47 -0700

    has been commited

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,13 @@
-
-#include "main.h"		
+#include "main.h"
 #include <stdlib.h>
-		
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: puts pointer to array, returns NULL if it fails	
- *	
- */
-		
+* create_array - create array of size size and assign char c
+* @size: size of array
+* @c: char to assign
+* Description: creat array of size size and assign char c
+* Return: puts pointer to array, returns NULL if it fails
+*
+*/
 char *create_array(unsigned int size, char c)
 
 {
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-		
 #include <stdlib.h>
-		
 #include "main.h"
-		
 /**
- * _strdup - duplicates to new memory space location	
- * @str: char	
- * Return: 0		
- */
-		
+* _strdup - duplicates to new memory space location
+* @str: char
+* Return: 0
+*
+*/
 char *_strdup(char *str)
-		
 {
-	char *aaa;	
+	char *aaa;
 	int i, r = 0;
-
 
 	if (str == NULL)
 
