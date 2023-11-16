46d41629db3be14df3b5f864858ac10d9d52b61e
EgundeyiSamuel <s.egundeyi@gmail.com>
2023-11-16 08:52:10 -0800

    add: print list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,27 +3,29 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
-* print_custom_list - Prints the elements of a custom list.
-* @custom_node: Pointer to the head of the custom list.
-* Return: The number of nodes in the list.
-*/
-size_t print_custom_list(const list_t *custom_node)
-{
-int node_count = 0;
 
-while (custom_node)
+/**
+* print_list - check the code for Holberton School students.
+* @h: name of the list
+* Return: the number of nodes.
+*/
+size_t print_list(const list_t *h)
 {
-if (custom_node->str == NULL)
+int count = 0;
+
+
+while (h)
+{
+if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 }
 else
 {
-printf("[%d] %s\n", custom_node->len, custom_node->str);
+printf("[%d] %s\n", h->len, h->str);
 }
-node_count++;
-custom_node = custom_node->next;
+count++;
+h = h->next;
 }
-return (node_count);
+return (count);
 }
